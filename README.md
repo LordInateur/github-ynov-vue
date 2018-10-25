522d30c1e0a5040463667ecf84a5404bc8cfd1e5
Matthieu Fournier <mat.fournier85@gmail.com>
2018-10-25 13:51:58 +0200

    pas de rechargement de lisrtOfRepo quand un object de gitusers est modifier

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # github-ynov-vue
 
-preview on : https://htmlpreview.github.io/?https://github.com/LordInateur/github-ynov-vue/blob/master/index.html
+preview on : http://mat-fournier.pagesperso-orange.fr/ynov-gitfollower/index.html
--- index.js
+++ index.js
@@ -1,30 +1,15 @@
 var app = new Vue({
   el: '#app',
-  /*  data: {
-    title: 'Titre : Une voiture',
-
-    form : { 
-      marque : "Mercedes",
-      modele : "CLK",
-      annee : 1995
-    },
-    voiture : {
-      marque : "Mercedes",
-      modele : "CLK",
-      annee : 1995
-    }
-    
-
-  }
-  */
+  
   created: function(){
     //created
   },
   mounted: function(){
     //
+    this.updateToken()
   },
   updated: function(){
-    
+    this.listOfRepo()
   },
   destroyed: function(){
 
@@ -35,11 +20,33 @@ var app = new Vue({
       max : new Date().toJSON().slice(0,10)
     },
     gitusers : [
-      {pseudo : "LordInateur", repos : ["github-ynov-vue"], repoSelected:["github-ynov-vue"]},
-      {pseudo : "benjaminbra", repos : ["github-ynov-vue"], repoSelected:["github-ynov-vue"]},
+      {pseudo : "LordInateur", repos : ["github-ynov-vue","SHI-FU-MI-clicker"], repoSelected:["github-ynov-vue"]},
+      {pseudo : "benjaminbra", repos : ["github-ynov-vue"], repoSelected:[]},
+      {pseudo : "Nair0fl", repos : ["github-ynov-vue"], repoSelected:[]},
+      {pseudo : "mathiasLoiret", repos : ["github-ynov-vue"], repoSelected:[]},
+      {pseudo : "thomaspich", repos : ["github-ynov-vue"], repoSelected:[]},
+      {pseudo : "TeofiloJ", repos : ["github-ynov-vue"], repoSelected:[]},
+      {pseudo : "Grigusky", repos : ["github-ynov-vue"], repoSelected:[]},
+      {pseudo : "Dakistos", repos : ["github-ynov-vue"], repoSelected:[]},
+      {pseudo : "mael61", repos : ["github-ynov-vue"], repoSelected:[]},
+      {pseudo : "KevinPautonnier", repos : ["github-ynov-vue"], repoSelected:[]},
+      {pseudo : "BenoitCochet", repos : ["github-ynov-vue"], repoSelected:[]},
+      {pseudo : "sfongue", repos : ["github-ynov-vue"], repoSelected:[]},
+      {pseudo : "ClementCaillaud", repos : ["github-ynov-vue"], repoSelected:[]},
+      {pseudo : "gfourny", repos : ["github-ynov-vue"], repoSelected:[]},
+      {pseudo : "Mokui", repos : ["github-ynov-vue"], repoSelected:[]},
+      {pseudo : "AntoineGOSSET", repos : ["github-ynov-vue"], repoSelected:[]},
+      {pseudo : "etienneYnov", repos : ["github-ynov-vue"], repoSelected:[]},
+      {pseudo : "Coblestone", repos : ["github-ynov-vue"], repoSelected:[]},
+      {pseudo : "AlexDesvallees", repos : ["github-ynov-vue"], repoSelected:[]},
+      {pseudo : "rudy8530", repos : ["github-ynov-vue"], repoSelected:[]},
+      {pseudo : "Killy85", repos : ["github-ynov-vue"], repoSelected:[]},
+      {pseudo : "alixnzt", repos : ["github-ynov-vue"], repoSelected:[]},
     ],
     userSelected : ["LordInateur"],
-    commitsList : {}
+    commitsList : {},
+    display_removeToken : false,
+    oauth_token : "initialisation"
   },
   methods: {
     console : function () {
@@ -51,59 +58,127 @@ var app = new Vue({
     getRepoData : function ( user, repo){
       // lordToken : 2de59a91c8a5f7df2bdcd7-d31c485508878ec4c9
 
-      var myImage = document.querySelector('img');
+      if(this.oauth_token != undefined && this.oauth_token.length == 40){
+ 
+        var myImage = document.querySelector('img');
 
-      var myHeaders = new Headers();
-      myHeaders.append('Authorization', 'token ' + oauth_token);
-      myHeaders.append('Accept', 'application/vnd.github.v3+json');
+        var myHeaders = new Headers();
+        myHeaders.append('Authorization', 'token ' + this.oauth_token);
+        myHeaders.append('Accept', 'application/vnd.github.v3+json');
 
-      var myInit = { method: 'GET',
-                     headers: myHeaders,
-                     mode: 'cors',
-                     cache: 'default' };
+        var myInit = { method: 'GET',
+                       headers: myHeaders,
+                       mode: 'cors',
+                       cache: 'default' };
 
-      var myRequest = new Request(`https://api.github.com/repos/${user}/${repo}/commits`);
+        var myRequest = new Request(`https://api.github.com/repos/${user}/${repo}/commits`);
 
-      var thus = this;
+        var thus = this;
 
-      fetch(myRequest,myInit).then(function(response) {
-         return response.json().then(function(json) {
-          thus.commitsList[`${user}/${repo}`].commits = json;
-          thus.commitsList[`${user}/${repo}`].loading = false;
-          thus.$forceUpdate();
-        });
-      })
+        fetch(myRequest,myInit)
+        .then(function(response) {
+           return response.json().then(function(json) {
+            try {
+              thus.commitsList[`${user}/${repo}`].commits = json;
+              thus.commitsList[`${user}/${repo}`].loading = false;
+              thus.$forceUpdate();
+            }catch (e){}
+            
+          })
+        })
+        .catch ( function(error)  {
+          alert("Probleme lors de la Request.")
+          return error
+        })
+        
+      }else if(this.oauth_token != "initialisation"){
+        alert("Probleme sur le token de connexion")
+        this.localStorage.removeItem('oauth_token')
+      } else {
+        console.log("Probleme sur le token de connexion")
+        this.localStorage.removeItem('oauth_token')
+      }
+      
+
       
     },
     getData(){
-      return this.$data
+      let tmpData = JSON.parse(JSON.stringify(this.$data))
+      delete tmpData.commitsList
+      return tmpData
     },
     setData(data){
       Object.assign(this.$data, JSON.parse(data))
-    }
-  },
-  computed: {
+    },
     listOfRepo : function(){
-      return this.gitusers.reduce((acc, value)=>{
-        console.log(value.repoSelected)
-        for ( repo in value.repoSelected){
-          acc.push(`${value.pseudo}/${value.repoSelected[repo]}`)
-          let title = `${value.pseudo}/${value.repoSelected[repo]}`;
-          if(this.commitsList[title] == undefined){
-            this.commitsList[title] = { 'title': title, loading : true }
-            this.getRepoData(value.pseudo, value.repoSelected[repo])
+      return this.gitusers.reduce((acc, user)=>{
+        //console.log(user.repoSelected)
+        for ( i_repo in user.repos){
+          let title = `${user.pseudo}/${user.repos[i_repo]}`;
+          if(user.repoSelected.indexOf(user.repos[i_repo]) < 0 ){
+            //this.commitsList.splice(user.repoSelected.indexOf(user.repos[i_repo]),1)
+            console.log("remove : " + title)
+            delete this.commitsList[title]
+          }else{
+            acc.push(`${user.pseudo}/${user.repoSelected[i_repo]}`)
+            if(this.commitsList[title] == undefined){
+              this.commitsList[title] = { 'title': title, loading : true }
+              this.getRepoData(user.pseudo, user.repoSelected[i_repo])
+            }
+
           }
         }
-        console.log(this.commitsList)
+        //console.log(this.commitsList)
         return acc
       }, [])
     },
+    removeToken : function(){
+      this.localStorage.removeItem('oauth_token')
+      this.oauth_token = "Removed"
+      this.display_removeToken = false
+    },
+    updateToken : function(){
+      try {
+        this.oauth_token = localStorage.getItem('oauth_token');
+        console.log(this.oauth_token)
+        while(this.oauth_token == null || this.oauth_token == undefined || this.oauth_token.length != 40){
+          
+          this.oauth_token = prompt("Veuillez saisir votre token de connexion git", "")
+          
+        }
+        
+        console.log("Chargement du token effectue")
+        console.log(this.oauth_token)
+       
+        this.localStorage.setItem('oauth_token', this.oauth_token)
+        console.log("token -" + this.localStorage.getItem('oauth_token') + "- save in localStorage")
 
-    getCommitsList : function(){
-      this.listOfRepo ;
-      // TODO : ajouter pour supprimer les objects qui ne sont pas dans listOfRepo
-      return this.commitsList;
+      } catch (e) {
+        alert("Autoriser les cookies pour sauvegarder le token d'authentification")
+        try {
+          this.oauth_token = prompt("Veuillez saisir votre token de connexion git", "")
+          this.localStorage.setItem('oauth_token', oauth_token)
+        } catch (e){
+          this.oauth_token = prompt("Veuillez saisir votre token de connexion git", "")
+          alert("Token non sauvegarder.")
+        }
+      }
+      this.display_removeToken = !this.isToken
+      //this.$forceUpdate();
     }
+  },
+  computed: {
+    isToken : function(){
 
+      try {
+        this.display_removeToken = this.localStorage.getItem('oauth_token').length == 40
+      }catch (e){
+        this.display_removeToken = false;
+      }
+      return this.display_removeToken;
+    },
+    localStorage : function(){
+      return window.localStorage
+    }
   }
 })
